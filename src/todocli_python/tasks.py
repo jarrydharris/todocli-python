0d4c51309c4d27119eedd449b2063dfd1e6fbdae
jarrydharris <mail@jarrydharris.org>
2024-09-18 21:42:28 +1000

    🦶 feat: list uncompleted tasks

--- src/todocli_python/tasks.py
+++ src/todocli_python/tasks.py
@@ -1,24 +1,35 @@
 import click
 from storage import Task, init_db
-from sqlalchemy import insert
+from sqlalchemy import Engine, insert, select
 
 
 @click.group()
 @click.pass_context
-def cli(ctx):
+def cli(ctx) -> None:
     ctx.ensure_object(dict)
-    ctx.obj["engine"] = init_db()
+    engine: Engine = init_db()
+    ctx.obj["engine"] = engine
 
 
 @cli.command()
 @click.argument("description")
 @click.pass_context
 def add(ctx, description: str) -> None:
-    """tasks add <description>"""
+    """tasks add \"Task description\" """
     with ctx.obj["engine"].connect() as conn:
         stmt = insert(Task).values(description=description)
         conn.execute(stmt)
-        conn.commit()
+
+
+@cli.command
+@click.pass_context
+def list(ctx) -> None:
+    """tasks list"""
+    with ctx.obj["engine"].connect() as conn:
+        stmt = select(Task.__table__)
+        tasks = conn.execute(stmt).fetchall()
+    for task in tasks:
+        print(*task)
 
 
 if __name__ == "__main__":
